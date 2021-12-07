257093851af096131baaf4a1d870d43a9f6929c9
Trong <trong.pham@enlabsoftware.com>
2021-12-08 01:24:21 +0700

    Try again

--- AWSServerless1/Startup.cs
+++ AWSServerless1/Startup.cs
@@ -14,43 +14,43 @@ using Microsoft.Extensions.Logging;
 
 namespace AWSServerless1
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public static IConfiguration Configuration { get; private set; }
+		public static IConfiguration Configuration { get; private set; }
 
-        // This method gets called by the runtime. Use this method to add services to the container
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-        }
+		// This method gets called by the runtime. Use this method to add services to the container
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllers();
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
 
-            app.UseHttpsRedirection();
+			app.UseHttpsRedirection();
 
-            app.UseRouting();
+			app.UseRouting();
 
-            app.UseAuthorization();
+			app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Welcome to running ASP.NET Core on AWS Lambda");
-                });
-            });
-        }
-    }
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+				endpoints.MapGet("/", async context =>
+				{
+					await context.Response.WriteAsync("Welcome to running ASP.NET Core on AWS Lambda");
+				});
+			});
+		}
+	}
 }
